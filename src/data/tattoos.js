e361bf16006021b0854975bc86547751a1f2b907
Phumrapee Soenvanichakul <77825705+Jannnn1235@users.noreply.github.com>
2021-09-23 19:03:33 +0700

    Add files via upload

---
+++ src/data/tattoos.js
@@ -0,0 +1,49 @@
+const tattoos = [
+    {
+        title: "cat1",
+        thumbnailUrl: "/images/cat1.jpg",
+        fullUrl: "/images/cat1.jpg"
+    },
+    {
+        title: "buffalo1",
+        thumbnailUrl: "/images/buffalo1.jpg",
+        fullUrl: "/images/buffalo1.jpg"
+    },
+    {
+        title: "elephant1",
+        thumbnailUrl: "/images/elephant2.jpg",
+        fullUrl: "/images/elephant2.jpg"
+    },
+    {
+        title: "cat2",
+        thumbnailUrl: "/images/cat2.jpg",
+        fullUrl: "/images/cat2.jpg"
+    },
+    {
+        title: "dog1",
+        thumbnailUrl: "/images/dog2.jpg",
+        fullUrl: "/images/dog2.jpg"
+    },
+    {
+        title: "buffalo2",
+        thumbnailUrl: "/images/buffalo2.jpg",
+        fullUrl: "/images/buffalo2.jpg"
+    },
+    {
+        title: "cat3",
+        thumbnailUrl: "/images/cat3.jpg",
+        fullUrl: "/images/cat3.jpg"
+    },
+    {
+        title: "dog2",
+        thumbnailUrl: "/images/dog1.jpg",
+        fullUrl: "/images/dog1.jpg"
+    },
+    {
+        title: "elephant2",
+        thumbnailUrl: "/images/elephant1.jpg",
+        fullUrl: "/images/elephant1.jpg"
+    }
+];
+
+export default tattoos;
